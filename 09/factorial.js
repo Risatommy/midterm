af0dae4bb32ff41efe4fc97c9f74470fa99ad17b
Risatommy <risa555@gmail.com>
2014-06-05 12:30:48 +0900

    難しですね(´･-･`)

--- 09/factorial.js
+++ 09/factorial.js
@@ -1,20 +1,6 @@
-function factorial(num){
-		if(num>=1){
-			result1 = 1;
-			for(i=1;i<=num;i++){
-				result1=result1*i;
-			}
-		}
-		
-		if(num>=0){
-			Math.floor(num);
-			result2 = 1;
-			for(i=1;i<=num;i++){
-				result2=result2*i;
-			}
-		}
-		
-		if(num<=0){
-			return null;
-		}
-	}
+/*
+
+(´･-･`)(´･-･`)(´･-･`)　
+
+
+*/
